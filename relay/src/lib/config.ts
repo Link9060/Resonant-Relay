90a0cf081e7bb9ba73bf0362e6ce1cccddd47e73
Lil Boy <153758512+Link9060@users.noreply.github.com>
2026-09-11 16:43:21 -0500

    Promote public to 1.0.3 and start beta 1.0.4

--- relay/src/lib/config.ts
+++ relay/src/lib/config.ts
@@ -1,8 +1,8 @@
 const isGitHubPages = process.env.NEXT_PUBLIC_RELAY_DEPLOY_TARGET === 'github-pages';
 
 export const IS_BETA = isGitHubPages;
-export const PRODUCTION_VERSION = '1.0.2';
-export const DEVELOPMENT_VERSION = '1.0.3';
+export const PRODUCTION_VERSION = '1.0.3';
+export const DEVELOPMENT_VERSION = '1.0.4';
 export const APP_VERSION = IS_BETA ? DEVELOPMENT_VERSION : PRODUCTION_VERSION;
 export const RELEASE_LABEL = IS_BETA ? `Beta ${APP_VERSION}` : APP_VERSION;
 export const APP_TITLE = IS_BETA ? `Relay · Beta ${APP_VERSION}` : `Relay · ${APP_VERSION}`;
